670b822eb0759aacc48a3564d4af6009d0fede8d
Mann Patel <130435633+MannPatel0@users.noreply.github.com>
2025-01-20 12:41:44 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -29,9 +29,9 @@ app.use(
 );
 
 let leet = require("./leetcode");
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.get("/problems", (req, res) => {
   res.json(data);
