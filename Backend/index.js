32ed5c712ff75470dad4516d180c64e647ae9640
Mann Patel <130435633+MannPatel0@users.noreply.github.com>
2025-01-20 14:13:03 -0700

    status
    return status 200 on json returning

--- Backend/index.js
+++ Backend/index.js
@@ -29,12 +29,9 @@ app.use(
 );
 
 let leet = require("./leetcode");
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
 
 app.get("/", (req, res) => {
-  res.json(data);
+  res.status(200).json(data);
 });
 
 app.get("/:id", specificLimiter, leet.leetcode);
